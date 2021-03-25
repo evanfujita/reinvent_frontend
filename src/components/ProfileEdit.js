da009ab4d8e3476418c427d44a47ba95e58678c5
Evan <evanfujita@gmail.com>
2021-03-25 15:49:37 -0500

    delete user complete

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { updateUser } from '../actions/user'
+import { logout } from '../actions/user'
 
 class ProfileEdit extends React.Component {
     
@@ -9,7 +10,8 @@ class ProfileEdit extends React.Component {
         first_name: '',
         last_name: '',
         username: '',
-        restaurant_name: ''
+        restaurant_name: '',
+        confirmDelete: false
     }
 
     handleChange = event => {
@@ -29,7 +31,7 @@ class ProfileEdit extends React.Component {
             }
         }
 
-        this.updateFetch(updatedUser, id)
+        // this.updateFetch(updatedUser, id)
 
         this.props.updateUser(updatedUser)
         this.setState({
@@ -53,12 +55,37 @@ class ProfileEdit extends React.Component {
         fetch(`http://localhost:3000/users/${id}`, reqObj)
         .then(resp => resp.json())
         .then(user => {
-            debugger
+            
         })
+    }
+
+    handleDelete = () => {
+        this.setState({
+            confirmDelete: !this.state.confirmDelete
+        })
+    }
+
+    confirmDelete = () => {
+        const { id } = this.props.user
+        fetch(`http://localhost:3000/users/${id}`, {method: 'DELETE'})
+        this.props.logout()
+        this.props.history.push('/login')
     }
     
     render(){
         const { first_name, last_name, username, restaurant_name } = this.props.user
+        const confirmDelete = this.state.confirmDelete 
+            ? 
+            <>
+            <Button color='red' onClick={this.confirmDelete}>Really Delete</Button><br/><br/>
+            <Button onClick={this.handleDelete}>Cancel</Button>
+            </>
+            : 
+            <>
+            <Button type='submit' onClick={this.handleSubmit}>Update Information</Button><br/><br/>
+            <Button color='red' onClick={this.handleDelete}>Delete</Button>
+            </>
+
         return(
             <div>
                 <header>
@@ -82,7 +109,8 @@ class ProfileEdit extends React.Component {
                         Restaurant Name
                         <input id='restaurant_name' onChange={this.handleChange} placeholder={restaurant_name} value={this.state.restaurant_name}/>
                     </Form.Field>
-                    <Button type='submit' onClick={this.handleSubmit}>Update Information</Button>
+                    
+                    {confirmDelete}
                 </Form>
             </div>
         )
