9092506f0610744ed85e1a3b99b5d28d83a57690
Evan <evanfujita@gmail.com>
2021-05-12 22:08:55 -0500

    CategoryMenuBar to functional component, imported hooks for selector, dispatch

--- src/components/Categories/CategoryMenuBar.js
+++ src/components/Categories/CategoryMenuBar.js
@@ -1,38 +1,35 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { selectCategory } from '../../actions/selections'
 import { Menu } from 'semantic-ui-react'
 
-class CategoryMenuBar extends React.Component {
-    handleClick = event => {
+const CategoryMenuBar = props => {
+
+    const selectedCategory = useSelector(state => state.selections.category)
+    const categories = useSelector(state => state.categories)
+    const dispatch = useDispatch()
+
+    const handleClick = event => {
         const id = parseInt(event.target.id)
-        this.props.selectCategory(id)
+        dispatch(selectCategory(id))
     }
     
-    render(){
-        const categoryId = this.props.category
-        const displayCategories = this.props.categories.map(category => {
-            return(<Menu.Item key={category.id} name={category.name} id={category.id} active={categoryId === category.id} onClick={this.handleClick} />)
-        })
-
-        return(
-            <Menu pointing secondary vertical>
-                <Menu.Item name={'All'} id={0} active={categoryId === 0} onClick={this.handleClick} />
-                {displayCategories}
-            </Menu>   
-        )
-    }
+    return(
+        <Menu pointing secondary vertical>
+            <Menu.Item name={'All'} id={0} active={selectedCategory === 0} onClick={handleClick} />
+            {
+                categories.map(category => (
+                    <Menu.Item 
+                        key={category.id} 
+                        name={category.name} 
+                        id={category.id} 
+                        active={selectedCategory === category.id} 
+                        onClick={handleClick} 
+                    />
+                ))
+            }
+        </Menu>   
+    )
 }
 
-const mapStateToProps = state => {
-    return {
-        categories: state.categories,
-        category: state.selections.category
-    }
-}
-
-const mapDispatchToProps = {
-    selectCategory
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CategoryMenuBar)
+export default CategoryMenuBar
