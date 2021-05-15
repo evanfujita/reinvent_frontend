9e320255cf985d55035a597225ef957cad8476f3
Evan <evanfujita@gmail.com>
2021-05-15 13:16:46 -0500

    updated PendingOrderContainer to functional component

--- src/components/Orders/OrderMeter.js
+++ src/components/Orders/OrderMeter.js
@@ -1,36 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'semantic-ui-react'
 
-class OrderMeter extends React.Component {
+const OrderMeter = props => {
 
-    state = { 
-        orderAbovePar: 0
-    }
+    const [orderAbovePar, setPar] = useState(0)
 
-    handleChange = event => {
-        this.setState({
-            orderAbovePar: event.target.value
-        })
+    const handleChange = event => {
+        setPar(event.target.value)
     }
     
-    render(){
-        const { orderAbovePar } = this.state
-        return(
-            <Form inverted>
-            <Form.Input
-                width='10'
-                label={`order relative to par: ${orderAbovePar}%`}
-                min={0}
-                max={300}
-                name='order relative to par'
-                onChange={this.handleChange}
-                step={5}
-                type='range'
-                value={orderAbovePar}
-            />
-            </Form>
-        )
-    }
+    return(
+        <Form inverted>
+        <Form.Input
+            width='10'
+            label={`order relative to par: ${orderAbovePar}%`}
+            min={0}
+            max={300}
+            name='order relative to par'
+            onChange={handleChange}
+            step={5}
+            type='range'
+            value={orderAbovePar}
+        />
+        </Form>
+    )
 }
 
 export default OrderMeter
--- src/components/Orders/PendingOrderContainer.js
+++ src/components/Orders/PendingOrderContainer.js
@@ -1,112 +1,89 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Menu, List, Grid, Message, Transition } from 'semantic-ui-react'
 import PendingOrderItem from './PendingOrderItem'
-import { acceptOrder } from '../../actions/pendingOrder'
+// import { acceptOrder } from '../../actions/pendingOrder'
 
-
-class PendingOrderContainer extends React.Component {
-    state = {
-        vendorId: 'all',
-        emailMessage: false,
-        acceptVisible: false,
-        rejectVisible: false,
-        item: ''
-    }
-
-    handleClick = event => {
+const PendingOrderContainer = props => {
+    //store
+    const pendingOrder = useSelector(state => state.pendingOrder)
+    const vendors = useSelector(state => state.vendors)
+    const itemsToAccept = useSelector(state => state.itemsToAccept)
+    const dispatch = useDispatch()
+    //local state
+    const [vendorId, setVendorId] = useState('all')
+    const [emailMessage, setEmailMessage] = useState(false)
+    const [acceptVisible, setAcceptVisible] = useState(false)
+    const [rejectVisible, setRejectVisible] = useState(false)
+    const [item, setItem] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    //methods
+    const handleClick = event => {
         const id = event.target.id
-        this.setState({
-            vendorId: id
-        })   
+        setVendorId(id)
     }
 
-    handleAcceptOrder = item => {
-        this.setState({ 
-            acceptVisible: true,
-            item: item
-        })
-        setTimeout(() => {this.setState({acceptVisible: false})}, 2000) 
+    const handleAcceptOrder = ingredient => {
+        setAcceptVisible(true)
+        setItem(ingredient)
+        setTimeout(() => {setAcceptVisible(false)}, 2000) 
     }
 
-    handleRejectOrder = item => {
-        this.setState({ 
-            rejectVisible: true,
-            item: item
-        })
-        setTimeout(() => {this.setState({rejectVisible: false})}, 2000) 
+    const handleRejectOrder = ingredient => {
+        setRejectVisible(true)
+        setItem(ingredient)
+        setTimeout(() => {setRejectVisible(false)}, 2000) 
     }
 
-
-    handleChange = event => {
+    const handleChange = event => {
         const id = parseInt(event.target.id)
 
         if(event.target.checked){
-            this.setState({
-                ingredients: [...this.state.ingredients, id]
-            })
+            setIngredients([...ingredients, id])
+            
         } else {
-            let newState = this.state.ingredients.filter(ingredient => ingredient !== id)
-            this.setState({
-                ingredients: newState
-            })
+            let newState = ingredients.filter(ingredient => ingredient !== id)
+            setIngredients(newState)
         }
     }
     
+    const categorizedIngredients = pendingOrder.filter(ingredient => (vendorId === ingredient.ingredient.vendor_id || vendorId === 'all' ? ingredient : null)        )
     
-    render(){
-        
-        const { acceptVisible, rejectVisible } = this.state
-        const vendorId = this.state.vendorId
-        const categorizedIngredients = this.props.pendingOrder.filter(ingredient => (vendorId === ingredient.ingredient.vendor_id || vendorId === 'all' ? ingredient : null)        )
-        
-        const vendorsMenu = this.props.vendors.map(vendor =>
-            <Menu.Item key={vendor.id} name={vendor.name} id={vendor.id} active={parseInt(vendorId) === vendor.id} onClick={this.handleClick} />
-        )
+    const vendorsMenu = vendors.map(vendor =>
+        <Menu.Item key={vendor.id} name={vendor.name} id={vendor.id} active={parseInt(vendorId) === vendor.id} onClick={handleClick} />
+    )
 
-        const displayIngredients = categorizedIngredients.map(ingredient => 
-            <PendingOrderItem 
-                key={ingredient.ingredient.id} 
-                ingredient={ingredient} 
-                handleAcceptOrder={this.handleAcceptOrder} 
-                handleRejectOrder={this.handleRejectOrder} 
-            /> 
-        )
-        
-        return(
-            <Grid>
-                <Grid.Column width={4} align='left'>
-                    <Menu align='left' className='text' pointing secondary vertical>
-                        By Vendor:
-                    <Menu.Item key='All' name='All' id='all' active={vendorId === 'all'} onClick={this.handleClick} />
-                        {vendorsMenu}
-                    </Menu>
+    const displayIngredients = categorizedIngredients.map(ingredient => 
+        <PendingOrderItem 
+            key={ingredient.ingredient.id} 
+            ingredient={ingredient} 
+            handleAcceptOrder={handleAcceptOrder} 
+            handleRejectOrder={handleRejectOrder} 
+        /> 
+    )
+    
+    return(
+        <Grid>
+            <Grid.Column width={4} align='left'>
+                <Menu align='left' className='text' pointing secondary vertical>
+                    By Vendor:
+                <Menu.Item key='All' name='All' id='all' active={vendorId === 'all'} onClick={handleClick} />
+                    {vendorsMenu}
+                </Menu>
 
-                </Grid.Column>
-                <Grid.Column width={8} align='left' className='scrollable'>
-                    <List>
-                        {displayIngredients}
-                    </List>
-                </Grid.Column>
-                <Grid.Column width={4}>
-                    <Transition visible={acceptVisible} animation='scale' duration={500}><Message size='mini' color='green'>{this.state.item} Accepted</Message></Transition>  
-                    <Transition visible={rejectVisible} animation='scale' duration={500}><Message size='mini' color='red'>{this.state.item} Rejected</Message></Transition>  
-                </Grid.Column>
-            </Grid>
-        )
-    }
+            </Grid.Column>
+            <Grid.Column width={8} align='left' className='scrollable'>
+                <List>
+                    {displayIngredients}
+                </List>
+            </Grid.Column>
+            <Grid.Column width={4}>
+                <Transition visible={acceptVisible} animation='scale' duration={500}><Message size='mini' color='green'>{item} Accepted</Message></Transition>  
+                <Transition visible={rejectVisible} animation='scale' duration={500}><Message size='mini' color='red'>{item} Rejected</Message></Transition>  
+            </Grid.Column>
+        </Grid>
+    )
+    
 }
 
-const mapStateToProps = state => {
-    return{
-        pendingOrder: state.pendingOrder,
-        vendors: state.vendors,
-        itemsToAccept: state.itemsToAccept
-    }
-}
-
-const mapDispatchToProps = {
-    acceptOrder
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PendingOrderContainer)
+export default PendingOrderContainer
