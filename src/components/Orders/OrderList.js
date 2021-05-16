bf91b837bdb89ca04925f174100c399a695cab54
Evan <evanfujita@gmail.com>
2021-05-15 20:08:46 -0500

    OrderList to functional component

--- src/components/Orders/OrderList.js
+++ src/components/Orders/OrderList.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { List, Grid, Menu, Form, Button, Segment } from 'semantic-ui-react'
 import NotesForm from '../Forms/NotesForm'
 import Email from '../FunctionalComponents/Email'
@@ -7,124 +7,102 @@ import OrderListItem from './OrderListItem'
 import { parMeter } from '../../actions/index'
 import { selectVendor } from '../../actions/selections'
 
-class OrderList extends React.Component {
-    
-    state = {
-        vendorId: 'all',
-        ingredients: [],
-        orderAbovePar: 0,
-        notesForm: false,
-        notes: ''
+const OrderList = props => {
+    //local state
+    const [vendorId, setVendorId] = useState('all')
+    const [orderAbovePar, setOrderAbovePar] = useState(0)
+    const [notesForm, setNotesForm] = useState(false)
+    const [notes, setNotes] = useState('')
+
+    //redux
+    const ingredients = useSelector(state => state.ingredients)
+    const lowIngredients = useSelector(state => state.lowIngredients)
+    const vendors = useSelector(state => state.vendors)
+    const orders = useSelector(state => state.orders)
+    const itemsToOrder = useSelector(state => state.itemsToOrder)
+
+    const dispatch = useDispatch()
+
+    const handleClick = event => {
+        const changeVendor = vendors.find(vendor => vendor.id === parseInt(event.target.id))
+        setVendorId(event.target.id)
+        dispatch(selectVendor(changeVendor))
     }
 
-    handleClick = event => {
-        const vendor = this.props.vendors.find(vendor => vendor.id === parseInt(event.target.id))
-        this.setState({
-            vendorId: event.target.id
-        })
-        this.props.selectVendor(vendor)
-    }
-
-    handleMeterChange = event => {
+    const handleMeterChange = event => {
         const meter = parseInt(event.target.value)
-        this.setState({
-            orderAbovePar: meter
-        })
-        this.props.parMeter(meter)
+        setOrderAbovePar(meter)
+        dispatch(parMeter(meter))
     }
 
-    handleNote = event => {
-        this.setState({
-            notesForm: !this.state.notesForm
-        })
+    const handleNote = event => {
+        setNotesForm(!notesForm)
     }
 
-    handleSubmitNote = () => {
-        this.setState({
-            notesForm: false
-        })
+    const handleSubmitNote = () => {
+        setNotesForm(false)
     }    
 
-    handleNoteChange = event => {
-        this.setState({
-            notes: event.target.value
-        })
+    const handleNoteChange = event => {
+        setNotes(event.target.value)
     }
     
-    render(){
-        const { orderAbovePar, vendorId, notes, notesForm } = this.state
-        const { lowIngredients, vendors, itemsToOrder } = this.props
-        const vendorInfo = vendors.find(vendor => parseInt(vendorId) === vendor.id)
+    const vendorInfo = vendors.find(vendor => parseInt(vendorId) === vendor.id)
+    
+    const categorizedIngredients = lowIngredients.filter(ingredient => vendorId === ingredient.vendor_id || vendorId === 'all' ? ingredient : null)
+    const renderNotesForm = notesForm ? <NotesForm handleNoteChange={handleNoteChange} /> : null
+    
+    const vendorsMenu = vendors.map(vendor => (
+        <Menu.Item key={vendor.id} name={vendor.name} id={vendor.id} active={parseInt(vendorId) === parseInt(vendor.id)} onClick={handleClick} />) )
         
-        const categorizedIngredients = lowIngredients.filter(ingredient => vendorId === ingredient.vendor_id || vendorId === 'all' ? ingredient : null)
-        const renderNotesForm = notesForm ? <NotesForm handleNoteChange={this.handleNoteChange} /> : null
+    const vendorIngredients = itemsToOrder.filter(ingredient => ingredient.ingredient.vendor_id === vendorId)
+    const displayIngredients = categorizedIngredients.map(ingredient => 
+        <OrderListItem key={ingredient.id} ingredient={ingredient}  />//addIngredient={addIngredient} />
+    )
+    const renderButtons = vendorId !== 'all' ? 
+        <>
+        <Button onClick={handleNote}>Add Note</Button><br/><br/>
+        <Email vendor={vendorInfo} ingredients={vendorIngredients} vendorId={vendorId} notes={notes} handleSubmit={handleSubmitNote} />
+        </>
+        :
+        null
         
-        const vendorsMenu = vendors.map(vendor => (
-            <Menu.Item key={vendor.id} name={vendor.name} id={vendor.id} active={parseInt(vendorId) === parseInt(vendor.id)} onClick={this.handleClick} />) )
-            
-        const vendorIngredients = itemsToOrder.filter(ingredient => ingredient.ingredient.vendor_id === vendorId)
-        const displayIngredients = categorizedIngredients.map(ingredient => 
-            <OrderListItem key={ingredient.id} ingredient={ingredient} addIngredient={this.addIngredient} />
-        )
-        const renderButtons = vendorId !== 'all' ? 
-            <>
-            <Button onClick={this.handleNote}>Add Note</Button><br/><br/>
-            <Email vendor={vendorInfo} ingredients={vendorIngredients} vendorId={vendorId} notes={notes} handleSubmit={this.handleSubmitNote} />
-            </>
-            :
-            null
-            
-        return(
-            <Grid>
-                <Grid.Column width='4' align='left'>
-                    <Menu align='left' className='text' pointing secondary vertical>
-                        By Vendor:
-                    <Menu.Item key='All' name='All' id='all' active={vendorId === 'all'} onClick={this.handleClick} />
-                            {vendorsMenu}
-                    </Menu>
-                </Grid.Column>
-                <Grid.Column width={8} align='left' className='scrollable'>
-                    <List>
-                        {displayIngredients}
-                    </List>
-                </Grid.Column>
-                <Grid.Column width={4}>
-                <Form inverted>
-                    <Segment>
-                    <Form.Input
-                        label={`order above par: ${orderAbovePar}%`}
-                        min={0}
-                        max={300}
-                        name='order relative to par'
-                        onChange={this.handleMeterChange}
-                        step={5}
-                        type='range'
-                        value={orderAbovePar}
-                        />
-                    
-                    {renderButtons}
-                    {renderNotesForm}
-                    </Segment>
-                    </Form>
-                </Grid.Column>
-            </Grid>
-        )
-    }
+    return(
+        <Grid>
+            <Grid.Column width='4' align='left'>
+                <Menu align='left' className='text' pointing secondary vertical>
+                    By Vendor:
+                <Menu.Item key='All' name='All' id='all' active={vendorId === 'all'} onClick={handleClick} />
+                        {vendorsMenu}
+                </Menu>
+            </Grid.Column>
+            <Grid.Column width={8} align='left' className='scrollable'>
+                <List>
+                    {displayIngredients}
+                </List>
+            </Grid.Column>
+            <Grid.Column width={4}>
+            <Form inverted>
+                <Segment>
+                <Form.Input
+                    label={`order above par: ${orderAbovePar}%`}
+                    min={0}
+                    max={300}
+                    name='order relative to par'
+                    onChange={handleMeterChange}
+                    step={5}
+                    type='range'
+                    value={orderAbovePar}
+                    />
+                
+                {renderButtons}
+                {renderNotesForm}
+                </Segment>
+                </Form>
+            </Grid.Column>
+        </Grid>
+    )
+    
 }
 
-const mapStateToProps = state => {
-    return {
-        ingredients: state.ingredients,
-        lowIngredients: state.lowIngredients,
-        vendors: state.vendors,
-        orders: state.orders,
-        itemsToOrder: state.itemsToOrder
-    }
-}
-
-const mapDispatchToProps = {
-    parMeter,
-    selectVendor
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(OrderList)
+export default OrderList
