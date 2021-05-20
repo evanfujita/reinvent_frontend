aab8fa5b5b412cf8b97b506702a7a939f0d4ef74
Evan <evanfujita@gmail.com>
2021-05-19 21:14:53 -0500

    added undo add item to order via reducer conditions

--- src/reducers/itemsToOrder.js
+++ src/reducers/itemsToOrder.js
@@ -1,14 +1,20 @@
 const itemsToOrder = (state=[], action) => {
+    let newState
     switch(action.type){
         case 'ITEM_TO_ORDER':
-            // debugger
-            return [
-                ...state,
-                action.ingredient
-            ]
+            let item = state.find(ingredient => (ingredient.ingredient.id === action.ingredient.ingredient.id))
+            if(item){
+                newState = state.filter(ingredient => ingredient.ingredient.id !== action.ingredient.ingredient.id)
+                return newState
+            } else {
+                return [
+                    ...state,
+                    action.ingredient
+                ]
+            }
         case 'DELETE_ITEM':
             // debugger
-            const newState = state.filter(ingredient => ingredient.ingredient.id !== action.ingredient.id)
+            newState = state.filter(ingredient => ingredient.ingredient.id !== action.ingredient.id)
             return newState
         default:
             return state
