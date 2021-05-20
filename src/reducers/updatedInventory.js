edb43a6b4ad430b1b4889f11fee7cef700bdab45
Evan <evanfujita@gmail.com>
2021-05-19 21:00:46 -0500

    added conditionals to updated ingredient action to dismiss duplicates

--- src/reducers/updatedInventory.js
+++ src/reducers/updatedInventory.js
@@ -1,11 +1,20 @@
 function updatedInventory (state=[], action) {
     
+    let newState
     switch(action.type){
         case 'UPDATED_INGREDIENT_INVENTORY':
-            return [...state, action.ingredient]
-        
+            let updatedIngredient = state.find(ingredient => ingredient.id === action.ingredient.id)
+            
+            if(updatedIngredient){
+                if(updatedIngredient.quantity !== action.ingredient.quantity){
+                    updatedIngredient.quantity = action.ingredient.quantity
+                } else {return state}
+            } else {
+                return [...state, action.ingredient]
+            }
+                    
         case 'UNDO_UPDATED_INGREDIENT_INVENTORY':
-            let newState = state.filter(i => i.id !== action.id)
+            newState = state.filter(i => i.id !== action.id)
             return newState
 
         case 'LOGOUT':
